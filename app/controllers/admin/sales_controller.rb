849ed4b2ba9d212c56ed88793c8193333076f371
Shivani Sharma <shivani1406sharma@gmail.com>
2022-02-05 03:00:10 +0000

    added sales view

--- app/controllers/admin/sales_controller.rb
+++ app/controllers/admin/sales_controller.rb
@@ -2,4 +2,14 @@ class Admin::SalesController < ApplicationController
   def index
     @sales = Sale.all
   end
+
+  def sale_params
+    params.require(:sale).permit(
+      :name,
+      :starts_on,
+      :ends_on,
+      :percent_off,
+      
+    )
+  end
 end
