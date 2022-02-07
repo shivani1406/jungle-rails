28657b0b65848604f9a9cd34efeac8b6e750dc3a
Shivani Sharma <shivani1406sharma@gmail.com>
2022-02-07 12:45:00 +0000

    added tests to user_spec

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -6,19 +6,50 @@ RSpec.describe User, type: :model do
       user = User.new(
         name: "test",
         email: "test@test.com",
-        password_digest: "test1"
+        password: "test1",
+        password_confirmation: "test1"
         )
       expect(user).to (be_valid)
+    end
+
+    it 'should check if password and password confirmation same' do
+      user = User.new(
+        name: "test",
+        email: "test@test.com",
+        password: "test1",
+        password_confirmation: "test1"
+        )
+      expect(user.password).to eq(user.password_confirmation)
+    end
+
+    it 'should check if password and password confirmation not same' do
+      user = User.new(
+        name: "test",
+        email: "test@test.com",
+        password: "test1",
+        password_confirmation: nil
+        )
+      expect(user.password).not_to eq(user.password_confirmation)
     end
 
     it 'should not save a invalid user password length less than 5' do
       user = User.new(
         name: "test",
         email: "test@test.com",
-        password_digest: "test"
+        password: "test"
         )
       expect(user).to (be_invalid)
     end
+
+    # it 'should not save a user if already in database' do
+    #   user = User.new(
+    #     name: "test",
+    #     email: "shivani1406sharma@gmail.com",
+    #     password: "test1",
+    #     password_confirmation: "test1"
+    #     )
+    #   expect(user).to (be_valid)
+    # end
 
   end
 end
